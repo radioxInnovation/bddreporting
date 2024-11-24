7f2a5eabee94fb2dbf4f795fa5248ed0c598c89a
frettig-radiox <118481987+frettig-radiox@users.noreply.github.com>
2024-11-24 19:26:45 +0100

    Update config.py

--- bddreporting/config.py
+++ bddreporting/config.py
@@ -1,48 +1,125 @@
-import os
-import json
+import jsonschema
 from jsonschema import validate as validate_json
 
 class Config:
-    _settings = None
-    _schema = None
-    _base_dir = os.path.dirname(os.path.abspath(__file__))   
+    _settings = {
+        "report_tag": None,
+        "report_dir": "reports/{{filename}}/",
+        "report_file_basename": "{{scenario}}",
+        "process_gherkin_doc_string": True,
+        "scenario_header": "## {{scenario}}\n\n",
+        "feature_header": "# {{feature}}\n\n",
+        "export_scenario": False,
+        "halt_execution_on_failure": False,
+        "default_formats": [ "docx", "txt"],
+        "formats": {
+            "docx": {
+                "extra_args": [
+                    "--toc"
+                ]
+            },
+            "pptx": {
+                "extra_args": [
+                    "--toc"
+                ]
+            },
+            "html": {
+                "extra_args": []
+            },
+            "md": {
+                "extra_args": []
+            },
+            "txt": {
+                "to": "plain",
+                "extra_args": []
+            }
+        }
+    }
 
-    @classmethod
-    def __load_defaults(cls):
-        if cls._settings is None:     
-            settings_file_path = os.path.join( cls._base_dir, "data", "settings.json" )
-            if os.path.isfile(settings_file_path):
-                with open(settings_file_path, "r") as file:
-                    settings = json.load(file)
-            cls._settings = settings
-        cls.__validate()
+    _schema = {
+        "type": "object",
+        "properties": {
+            "report_tag": {
+                "type": ["string", "null"],
+                "description": "Tag used to identify reports. null means no filter, report every scenario"
+            },
+            "report_dir": {
+                "type": "string",
+                "description": "Directory where reports are stored"
+            },
+            "report_file_basename": {
+                "type": "string",
+                "description": "base name of a report file"
+            },
+            "process_gherkin_doc_string": {
+                "type": "boolean",
+                "description": "Flag to enable or disable processing of Gherkin doc strings"
+            },
+            "scenario_header": {
+                "type": ["string", "boolean", "null"],
+                "description": "Template for the scenario header"
+            },
+            "feature_header": {
+                "type": ["string", "boolean", "null"],
+                "description": "Template for the feature header"
+            },
+            "export_scenario": {
+                "type": "boolean",
+                "description": "export scenario"
+            },
+            "halt_execution_on_failure": {
+                "type": "boolean",
+                "description": "Flag to determine whether to exit on step failure"
+            },
+            "default_formats": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                    "pattern": "^[a-zA-Z0-9_]+$"
+                },
+                "description": "Default formats, must be null or an array of strings matching the keys in the formats dictionary."
+            },
+            "formats": {
+                "type": "object",
+                "description": "Extra arguments for different formats.",
+                "patternProperties": {
+                    "^[a-zA-Z0-9_]+$": {
+                    "type": "object",
+                    "description": "Additional arguments for any format."
+                    }
+                }
+            }
+        },
+        "required": [
+            "report_tag",
+            "report_dir",
+            "process_gherkin_doc_string",
+            "scenario_header",
+            "halt_execution_on_failure",
+            "default_formats",
+            "formats"
+        ],
+        "additionalProperties": False
+    }
 
     @classmethod
     def update(cls, **kwargs):
-        cls.__load_defaults( )
         cls._settings.update( kwargs )
         cls.__validate()
 
     @classmethod
     def get(cls, key, default=None):
-        cls.__load_defaults( )
+        cls.__validate()
         return cls._settings.get(key, default)
 
     @classmethod
     def all(cls):
-        cls.__load_defaults( )
+        cls.__validate()
         return cls._settings
 
     @classmethod
     def __validate( cls ):
         try:
-            schema_file_path = os.path.join( cls._base_dir, "data", "schema.json" )
-
-            # Load settings from JSON file if it exists, otherwise use default settings
-            if cls._schema is None and os.path.isfile(schema_file_path):
-                with open(schema_file_path, "r") as file:
-                    cls._schema = json.load(file)
-
             # Validate the JSON object against the schema
             validate_json(instance=cls._settings, schema=cls._schema)
             return True
